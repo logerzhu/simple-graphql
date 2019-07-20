f83aff7f96cb4dfe20689460afee30bca7d04b58
Loger <loger.zhu@gmail.com>
2019-07-21 00:56:29 +0800

    support save Mutation generation

--- src/plugin/index.js
+++ src/plugin/index.js
@@ -5,6 +5,7 @@ import deleteMutationPlugin from './deleteMutationPlugin'
 import genHasManyLinkPlugin from './genHasManyLinkPlugin'
 import genHasOneLinkPlugin from './genHasOneLinkPlugin'
 import pluralQueryPlugin from './pluralQueryPlugin'
+import saveMutationPlugin from './saveMutationPlugin'
 import singularQueryPlugin from './singularQueryPlugin'
 import timestampPlugin from './timestampPlugin'
 import updateMutationPlugin from './updateMutationPlugin'
@@ -16,6 +17,7 @@ export default [
   genHasManyLinkPlugin,
   genHasOneLinkPlugin,
   pluralQueryPlugin,
+  saveMutationPlugin,
   singularQueryPlugin,
   timestampPlugin,
   updateMutationPlugin,
---
+++ src/plugin/saveMutationPlugin.js
@@ -0,0 +1,79 @@
+// @flow
+import _ from 'lodash'
+import type { ColumnFieldOptions, PluginOptions } from '../Definition'
+import StringHelper from '../utils/StringHelper'
+
+export default ({
+  name: 'saveMutation',
+  defaultOptions: false,
+  priority: 0,
+  description: 'Gen `save mutation` for Schema',
+  apply: function (schema, options, schemas) {
+    const name = 'save' + StringHelper.toInitialUpperCase(schema.name)
+    const savedName = 'saved' + StringHelper.toInitialUpperCase(schema.name)
+
+    const inputFields = {}
+    const isModelType = (fieldOptions: ColumnFieldOptions) => {
+      if (typeof fieldOptions === 'string') {
+        return schemas.find(s => s.name === fieldOptions) != null
+      } else if (typeof fieldOptions === 'object') {
+        return schemas.find(s => s.name === (fieldOptions: any).$type) != null
+      }
+      return false
+    }
+    _.forOwn(schema.config.fields, (value, key) => {
+      if (isModelType(value)) {
+        if (!key.endsWith('Id')) {
+          key = key + 'Id'
+        }
+      }
+
+      if (value && value.$type) {
+        if (!value.hidden) {
+          inputFields[key] = value
+        }
+      } else {
+        inputFields[key] = value
+      }
+    })
+    let config = {}
+    if ((typeof options) === 'object') {
+      config = options
+    }
+    schema.mutations({
+      [config.name || name]: {
+        config: config,
+        inputFields: inputFields,
+        outputFields: {
+          [savedName]: schema.name
+        },
+        mutateAndGetPayload: async function (args, context, info, sgContext) {
+          const dbModel = sgContext.models[schema.name]
+          const attrs = {}
+
+          _.forOwn(schema.config.fields, (value, key) => {
+            if (isModelType(value)) {
+              if (!key.endsWith('Id')) {
+                key = key + 'Id'
+              }
+              if (typeof args[key] !== 'undefined') {
+                if (dbModel.options.underscored) {
+                  attrs[StringHelper.toUnderscoredName(key)] = args[key]
+                } else {
+                  attrs[key] = args[key]
+                }
+              }
+            } else if (typeof args[key] !== 'undefined') {
+              attrs[key] = args[key]
+            }
+          })
+
+          await dbModel.upsert(attrs)
+          return {
+            [savedName]: dbModel.findOne({ where: attrs })
+          }
+        }
+      }
+    })
+  }
+}: PluginOptions)
