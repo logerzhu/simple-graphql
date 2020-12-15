4ce0f222de197426e0071c78b732ad841b0e83e0
Loger <loger.zhu@gmail.com>
2020-12-15 15:26:56 +0800

    修复include.parent导致的递归引用问题

--- src/plugin/cache/getFindOptionsKey.ts
+++ src/plugin/cache/getFindOptionsKey.ts
@@ -3,8 +3,21 @@ import md5 from 'md5'
 
 export default ((dbModel, options: FindOptions | CountOptions) => {
   options = options || {};
+  const formatInclude = (include) => {
+    if (include && include.parent !== undefined) {
+      const {parent, ...other} = include
+      if (other.include && Array.isArray(other.include)) {
+        other.include = other.include.map(i => formatInclude(i))
+      }
+      return other
+    }
+    return include
+  }
+  const include = (options.include || []).map(i => formatInclude(i))
+
   return md5(JSON.stringify({
     ...options,
+    include: include,
     where: dbModel.QueryGenerator.whereQuery(options.where)
   }));
 });
