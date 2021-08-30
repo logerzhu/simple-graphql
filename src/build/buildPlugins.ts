4ab4197b6492e1fdf731bcc7a85ebcb11715ceb1
Loger <loger.zhu@gmail.com>
2021-08-30 12:55:51 +0800

    避免内置Plugin的实例共享问题

--- src/build/buildPlugins.ts
+++ src/build/buildPlugins.ts
@@ -2,19 +2,20 @@ import innerPlugins from '../plugin'
 import { SGPluginConfig } from '..'
 
 export default (plugins: Array<SGPluginConfig>): Array<SGPluginConfig> => {
-  return [...innerPlugins, ...plugins]
-    .sort((p1, p2) => {
-      const p1n = p1.priority || 0
-      const p2n = p2.priority || 0
-      if (p1n < p2n) {
-        return 1
-      } else if (p1n > p2n) {
-        return -1
-      } else {
-        return 0
-      }
-    })
-    .map((plugin) => {
+  return [
+    ...innerPlugins.map((plugin) => {
       return { ...plugin }
-    })
+    }),
+    ...plugins
+  ].sort((p1, p2) => {
+    const p1n = p1.priority || 0
+    const p2n = p2.priority || 0
+    if (p1n < p2n) {
+      return 1
+    } else if (p1n > p2n) {
+      return -1
+    } else {
+      return 0
+    }
+  })
 }
